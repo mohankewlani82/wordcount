01ab1ed357b6c617e6f6327d46a4652c40291baf
unknown <kewlanim@gmail.com>
2024-10-26 21:40:12 +0530

    cloned new repo

--- sample.py
+++ sample.py
@@ -2,4 +2,5 @@ print("Sample file created in git")
 
 happy = input("Enter a phrase to word counts:")
 happy = input("Enter a statement to check word frequency: ")
+ "Welcome to the wordcount repo!"
 
